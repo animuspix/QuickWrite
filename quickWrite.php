16fcc1ad49c4327bae730ac65a27c62a10db54c3
crappyWebDev <peferris@internode.on.net>
2015-12-01 17:29:43 +1100

    Fairly significant refactor
    Re-oriented the code so that the actual text echo can be done through a
    function call rather than including the whole file. Instead of writing
    "$file = 'xyz.txt'; include 'quickWrite.php';", users can include
    quickWrite once at the top of the file and use it by calling publisher
    with the file they want to publish, like so:
    
    <?php
    publisher('xyz.txt');
    ?>

--- quickWrite.php
+++ quickWrite.php
@@ -1,22 +1,9 @@
 <?php
-    $text = file_get_contents($file);
-    $textArray = array();
-    $textPusher;
-    $textArrayCrawler;
 
-    $postCrawler;
-    $italCount = 0;
-    $underlineCount = 0;
-    $boldCount = 0;
-    $strikeCount = 0;
-    $linkCount = 0;
-    $linkTextCount = 0;
-    $escapeCount = 0;
-    $target;
     $targetCounter;
 
-    $escapeArray = array("`", "|", "^", "*", "/", "~", "-", "<", ">");
-    $escapeCharCodes = array("&#96;", "&#124;", "&#94;", "&#42;", "&#47;", "&#126;", "&#45;", "&lt;", "&gt;");
+    $escapeArray = array("`", "|", "^", "*", "/", "~", "-", "<", ">", "+");
+    $escapeCharCodes = array("&#96;", "&#124;", "&#94;", "&#42;", "&#47;", "&#126;", "&#45;", "&lt;", "&gt;", "&#43;");
 
     function target($charConvert) {
         
@@ -30,94 +17,119 @@
             }
     }
 
-
-    for ($textPusher = 0; $textPusher < strlen($text); $textPusher += 1) {
+    function publisher($file) {
         
-        $currentChar = substr($text, $textPusher, 1);
+        $text = file_get_contents($file);
+        $textArray = array();
+        $textPusher;
+        $textArrayCrawler;
         
-        if ($currentChar === "~") {
-            $escapeCount += 1;   
-        }
+        $postCrawler = 0;
+        $italCount = 0;
+        $underlineCount = 0;
+        $boldCount = 0;
+        $strikeCount = 0;
+        $linkCount = 0;
+        $linkTextCount = 0;
+        $escapeCount = 0;
+        $headerCount = 0;
         
-        if ($escapeCount === 0) {
-            
-            if ($currentChar === "^" and $linkCount === 0) {
-                array_push($textArray, "<a href='");
-                $linkCount += 1;
+        for ($textPusher = 0; $textPusher < strlen($text); $textPusher += 1) {
 
-            } elseif ($currentChar === "`" and $linkCount === 1 and $linkTextCount === 0) {
-                array_push($textArray, "'>");
-                $linkTextCount += 1;
+            $currentChar = substr($text, $textPusher, 1);
 
-            } elseif ($currentChar === "`" and $linkCount === 1 and $linkTextCount === 1) {
-                array_push($textArray, "");
-
-            } elseif ($currentChar === "^" and $linkCount === 1 and $linkTextCount === 1) {
-                array_push($textArray, "</a>");
-                $linkCount = 0;
-                $linkTextCount = 0;
+            if ($currentChar === "~") {
+                $escapeCount += 1;   
             }
-            
-            if ($linkCount === 0) {
-                if ($currentChar === "/" and $italCount === 0) {
-                    array_push($textArray, "<em>"); 
-                    $italCount += 1;
 
-                } elseif ($currentChar === "/" and $italCount === 1){
-                    array_push($textArray, "</em>");
-                    $italCount = 0;
-                } 
+            if ($escapeCount === 0) {
 
-                if ($currentChar === "-" and $strikeCount === 0) {
-                    array_push($textArray, "<strike>");
-                    $strikeCount += 1;
+                if ($currentChar === "^" and $linkCount === 0) {
+                    array_push($textArray, "<a href='");
+                    $linkCount += 1;
 
-                } elseif ($currentChar === "-" and $strikeCount === 1) {
-                    array_push($textArray, "</strike>");
-                    $strikeCount = 0;
+                } elseif ($currentChar === "`" and $linkCount === 1 and $linkTextCount === 0) {
+                    array_push($textArray, "'>");
+                    $linkTextCount += 1;
+
+                } elseif ($currentChar === "`" and $linkCount === 1 and $linkTextCount === 1) {
+                    array_push($textArray, "");
+
+                } elseif ($currentChar === "^" and $linkCount === 1 and $linkTextCount === 1) {
+                    array_push($textArray, "</a>");
+                    $linkCount = 0;
+                    $linkTextCount = 0;
                 }
 
-                if ($currentChar === "_" and $underlineCount === 0) {
-                    array_push($textArray, "<u>");
-                    $underlineCount += 1;
+                if ($linkCount === 0) {
+                    if ($currentChar === "/" and $italCount === 0) {
+                        array_push($textArray, "<em>"); 
+                        $italCount += 1;
 
-                } elseif ($currentChar === "_" and $underlineCount === 1) {
-                    array_push($textArray, "</u>");
-                    $underlineCount = 0;
+                    } elseif ($currentChar === "/" and $italCount === 1){
+                        array_push($textArray, "</em>");
+                        $italCount = 0;
+                    } 
+
+                    if ($currentChar === "-" and $strikeCount === 0) {
+                        array_push($textArray, "<strike>");
+                        $strikeCount += 1;
+
+                    } elseif ($currentChar === "-" and $strikeCount === 1) {
+                        array_push($textArray, "</strike>");
+                        $strikeCount = 0;
+                    }
+
+                    if ($currentChar === "_" and $underlineCount === 0) {
+                        array_push($textArray, "<u>");
+                        $underlineCount += 1;
+
+                    } elseif ($currentChar === "_" and $underlineCount === 1) {
+                        array_push($textArray, "</u>");
+                        $underlineCount = 0;
+                    }
+
+                    if ($currentChar === "*" and $boldCount === 0) {
+                        array_push($textArray, "<strong>");
+                        $boldCount += 1;
+
+                    } elseif ($currentChar === "*" and $boldCount === 1) {
+                        array_push($textArray, "</strong>");
+                        $boldCount = 0;
+                    }
+
+                    if ($currentChar === "|") {
+                        array_push($textArray, "<br>");
+                    }
+
+                    if ($currentChar === "+") {
+                        array_push($textArray, "<h3>");
+                        $headerCount += 1;
+
+                    } elseif ($currentChar === "+" and $headerCount === 1) {
+                        array_push($textArray, "</h3>");
+                        $headerCount = 0;
+                    }
                 }
+            } elseif ($currentChar === "~" and $escapeCount > 1) {
+                array_push($textArray, target($currentChar));
 
-                if ($currentChar === "*" and $boldCount === 0) {
-                    array_push($textArray, "<strong>");
-                    $boldCount += 1;
-
-                } elseif ($currentChar === "*" and $boldCount === 1) {
-                    array_push($textArray, "</strong>");
-                    $boldCount = 0;
-                }
-
-                if ($currentChar === "|") {
-                    array_push($textArray, "<br>");
-                }
+            } elseif ($currentChar !== "~" and $escapeCount >= 1) {
+                array_push($textArray, target($currentChar));
+                $escapeCount = 0;
             }
-        } elseif ($currentChar === "~" and $escapeCount > 1) {
-            array_push($textArray, target($currentChar));
-            
-        } elseif ($currentChar !== "~" and $escapeCount >= 1) {
-            array_push($textArray, target($currentChar));
-            $escapeCount = 0;
+
+            if ($currentChar !== "_" and $currentChar !== "*" and $currentChar !== "/" and $currentChar !== "-" and $currentChar !== "`" and $currentChar !== "~" and $currentChar !== "|" and $currentChar !== "^" and $currentChar !== "<" and $currentChar !== ">") {
+                array_push($textArray, $currentChar);
+            }
         }
-        
-        if ($currentChar !== "_" and $currentChar !== "*" and $currentChar !== "/" and $currentChar !== "-" and $currentChar !== "`" and $currentChar !== "~" and $currentChar !== "|" and $currentChar !== "^" and $currentChar !== "<" and $currentChar !== ">") {
-            array_push($textArray, $currentChar);
+
+        echo "<p>";
+
+        for ($textArrayCrawler = 0; $textArrayCrawler < count($textArray); $textArrayCrawler += 1) {
+            echo $textArray[$textArrayCrawler] . "";
         }
+
+        echo "</p>";
     }
-
-    echo "<p>";
-
-    for ($textArrayCrawler = 0; $textArrayCrawler < count($textArray); $textArrayCrawler += 1) {
-        echo $textArray[$textArrayCrawler] . "";
-    }
-    
-    echo "</p>";
-
 ?>
